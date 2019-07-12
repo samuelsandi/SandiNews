b29b1229952ae063c2e21ce2a035cb9c725b2e26
Samuel Sandi Kristianto Lim <t-samuel.lim@ovo.id>
2019-07-12 13:10:28 +0700

    Basic interface design

---
+++ SandiNews/Controllers/NewsListTableViewController.swift
@@ -0,0 +1,13 @@
+//
+//  NewsListTableViewController.swift
+//  SandiNews
+//
+//  Created by OVO on 12/07/19.
+//  Copyright © 2019 Samuel Lim. All rights reserved.
+//
+
+import UIKit
+
+class NewsListTableViewController: UITableViewController {
+    
+}
